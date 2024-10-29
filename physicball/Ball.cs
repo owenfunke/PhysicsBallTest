49665bbd150356e1981a6afe7b3f494e806fa470
Owen Funke <owen.funke@gmail.com>
2024-10-29 13:10:44 -0500

    Removing jump

--- physicball/Ball.cs
+++ physicball/Ball.cs
@@ -3,9 +3,7 @@ using System;
 
 public partial class Ball : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
-
+	public const float pSpeed = 300.0f;
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
@@ -16,22 +14,17 @@ public partial class Ball : CharacterBody2D
 			velocity += GetGravity() * (float)delta;
 		}
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
-			velocity.X = direction.X * Speed;
+			velocity.X = direction.X * pSpeed * (float)delta;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, pSpeed);
 		}
 
 		Velocity = velocity;
