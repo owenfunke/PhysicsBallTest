d128644d24103d2079c7f44c95a3009518879c3f
Owen Funke <owen.funke@gmail.com>
2024-10-30 12:07:59 -0500

    Creating Physics

--- physicball/Main.cs
+++ physicball/Main.cs
@@ -1,20 +1,16 @@
 using Godot;
 using System;
 
-public partial class Balls : RigidBody2D
+public partial class Main : Node2D
 {
-	public const float initialVelocity = 20;
-	public const float initialX = 0;
-	public const float gravity = 9.8f;
-
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Timer timer = GetNode<Timer>("TimeCounter");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Position = initialX + initialVelocity * delta + 0.5f *  * delta * delta;
 	}
 }
