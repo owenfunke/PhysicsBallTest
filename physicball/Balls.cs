40a2a209efe0fb9d665f942081cc1cc06157e3a5
Owen Funke <owen.funke@gmail.com>
2024-10-30 12:15:57 -0500

    Small Physic Changes

--- physicball/Balls.cs
+++ physicball/Balls.cs
@@ -5,7 +5,8 @@ public partial class Balls : RigidBody2D
 {
 	public const float initialVelocity = 20;
 	public const float initialX = 0;
-	public const float gravity = 9.8f;
+	public const float gravity = -9.8f;
+	public float positionx = 0;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -15,6 +16,7 @@ public partial class Balls : RigidBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Position = initialX + initialVelocity * delta + 0.5f *  * delta * delta;
+		positionx = initialX + initialVelocity * ;
+		Position = new Vector2(positionx, Position.Y);
 	}
 }
